45138e67441a8834a03931757c61fad2020cee7e
Kyle Fredrickson <kyle.fredrickson@chicagobooth.edu>
2023-04-04 15:06:00 +0000

    Final

--- array_min_max_difference.rb
+++ array_min_max_difference.rb
@@ -9,3 +9,9 @@
 # NOTE: for this exercise use the provided array (don't ask the user for a value, in this task).
 
 array = [12, 23, 41, 73, 19, 6]
+min=array.min
+max=array.max
+dif=max-min
+p min
+p max
+p dif
